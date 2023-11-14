1bfd612385bd096be61df8c91e0971d07f5df435
louisaldorio <louisaldorio@gmail.com>
2023-11-14 10:44:41 +0700

    add get tracking number function

--- logistics.go
+++ logistics.go
@@ -1,12 +1,15 @@
 package goshopee
 
+import "strings"
+
 type LogisticsService interface {
 	GetChannelList(uint64, string) (*GetChannelListResponse, error)
-	GetShippingParameter(uint64, string,string) (*GetShippingParameterResponse, error)
+	GetShippingParameter(uint64, string, string) (*GetShippingParameterResponse, error)
+	GetTrackingNumber(uint64, string, []string, string) (*GetTrackingNumberResponse, error)
 	ShipOrder(uint64, ShipOrderRequest, string) (*ShipOrderResponse, error)
 }
 
-type LogisticsServiceOp struct{
+type LogisticsServiceOp struct {
 	client *Client
 }
 
@@ -22,24 +25,24 @@ type GetChannelListResponseData struct {
 }
 
 type LogisticsChannel struct {
-	LogisticsChannelID uint64 `json:"logistics_channel_id"`
-	LogisticsChannelName string `json:"logistics_channel_name"`
-	CODEnabled bool `json:"cod_enabled"`
-	Enabled bool `json:"enabled"`
-	FeeType string `json:"fee_type"`
-	SizeList []Size `json:"size_list"`
-	WeightLimit WeightLimit `json:"weight_limit"`
-	ItemMaxDimension ItemMaxDimension `json:"item_max_dimension"`
-	Preferred bool `json:"preferred"`
-	ForceEnabled bool `json:"force_enabled"` // TODO: infact in list
-	MaskChannelID uint64 `json:"mask_channel_id"` // TODO: infact no?
-	LogisticsDescription string `json:"logistics_description"` // TODO: infact no?
-	VolumeLimit VolumeLimit `json:"volume_limit"`
+	LogisticsChannelID   uint64           `json:"logistics_channel_id"`
+	LogisticsChannelName string           `json:"logistics_channel_name"`
+	CODEnabled           bool             `json:"cod_enabled"`
+	Enabled              bool             `json:"enabled"`
+	FeeType              string           `json:"fee_type"`
+	SizeList             []Size           `json:"size_list"`
+	WeightLimit          WeightLimit      `json:"weight_limit"`
+	ItemMaxDimension     ItemMaxDimension `json:"item_max_dimension"`
+	Preferred            bool             `json:"preferred"`
+	ForceEnabled         bool             `json:"force_enabled"`         // TODO: infact in list
+	MaskChannelID        uint64           `json:"mask_channel_id"`       // TODO: infact no?
+	LogisticsDescription string           `json:"logistics_description"` // TODO: infact no?
+	VolumeLimit          VolumeLimit      `json:"volume_limit"`
 }
 
 type Size struct {
-	SizeID string `json:"size_id"`
-	Name string `json:"name"`
+	SizeID       string  `json:"size_id"`
+	Name         string  `json:"name"`
 	DefaultPrice float64 `json:"default_price"`
 }
 
@@ -50,9 +53,9 @@ type WeightLimit struct {
 
 type ItemMaxDimension struct {
 	Height float64 `json:"height"`
-	Width float64 `json:"width"`
+	Width  float64 `json:"width"`
 	Length float64 `json:"length"`
-	Unit string `json:"unit"`
+	Unit   string  `json:"unit"`
 }
 
 type VolumeLimit struct {
@@ -60,11 +63,11 @@ type VolumeLimit struct {
 	ItemMinVolume float64 `json:"item_min_volume"`
 }
 
-func (s *LogisticsServiceOp)GetChannelList(sid uint64, tok string) (*GetChannelListResponse, error){
+func (s *LogisticsServiceOp) GetChannelList(sid uint64, tok string) (*GetChannelListResponse, error) {
 	path := "/logistics/get_channel_list"
 
 	resp := new(GetChannelListResponse)
-	err := s.client.WithShop(sid,tok).Get(path, resp, nil)
+	err := s.client.WithShop(sid, tok).Get(path, resp, nil)
 	return resp, err
 }
 
@@ -80,8 +83,8 @@ type GetShippingParameterResponse struct {
 
 type GetShippingParameterResponseData struct {
 	InfoNeeded GetShippingParameterResponseDataInfo `json:"info_needed"`
-	Dropoff Dropoff `json:"dropoff"`
-	Pickup Pickup `json:"pickup"`
+	Dropoff    Dropoff                              `json:"dropoff"`
+	Pickup     Pickup                               `json:"pickup"`
 }
 
 type Pickup struct {
@@ -89,21 +92,21 @@ type Pickup struct {
 }
 
 type LogisticsAddress struct {
-	AddressID uint64 `json:"address_id"`
-	Region string `json:"region"`
-	State string `json:"state"`
-	City string `json:"city"`
-	Address string `json:"address"`
-	Zipcode string `json:"zipcode"`
-	District string `json:"district"`
-	Town string `json:"town"`
-	AddressFlag []string `json:"address_flag"`
+	AddressID    uint64     `json:"address_id"`
+	Region       string     `json:"region"`
+	State        string     `json:"state"`
+	City         string     `json:"city"`
+	Address      string     `json:"address"`
+	Zipcode      string     `json:"zipcode"`
+	District     string     `json:"district"`
+	Town         string     `json:"town"`
+	AddressFlag  []string   `json:"address_flag"`
 	TimeSlotList []TimeSlot `json:"time_slot_list"`
 }
 
 type TimeSlot struct {
-	Date int64 `json:"date"`
-	TimeText string `json:"time_text"`
+	Date         int64  `json:"date"`
+	TimeText     string `json:"time_text"`
 	PickupTimeID string `json:"pickup_time_id"`
 }
 
@@ -113,37 +116,68 @@ type Dropoff struct {
 
 type Branch struct {
 	BranchID uint64 `json:"branch_id"`
-	Region string `json:"region"`
-	State string `json:"state"`
-	City string `json:"city"`
-	Address string `json:"address"`
-	Zipcode string `json:"zipcode"`
+	Region   string `json:"region"`
+	State    string `json:"state"`
+	City     string `json:"city"`
+	Address  string `json:"address"`
+	Zipcode  string `json:"zipcode"`
 	District string `json:"district"`
-	Town string `json:"town"`
+	Town     string `json:"town"`
 }
 
 type GetShippingParameterResponseDataInfo struct {
-	Dropoff []string `json:"dropoff"`
-	Pickup []string `json:"pickup"`
+	Dropoff       []string `json:"dropoff"`
+	Pickup        []string `json:"pickup"`
 	NonIntegrated []string `json:"non_integrated"`
 }
 
-func (s *LogisticsServiceOp)GetShippingParameter(sid uint64, ordersn, tok string) (*GetShippingParameterResponse, error){
+func (s *LogisticsServiceOp) GetShippingParameter(sid uint64, ordersn, tok string) (*GetShippingParameterResponse, error) {
 	path := "/logistics/get_shipping_parameter"
-	opt:=GetShippingParameterRequest{
+	opt := GetShippingParameterRequest{
 		OrderSN: ordersn,
 	}
 
 	resp := new(GetShippingParameterResponse)
-	err := s.client.WithShop(sid,tok).Get(path, resp, opt)
+	err := s.client.WithShop(sid, tok).Get(path, resp, opt)
+	return resp, err
+}
+
+type GetTrackingNumberRequest struct {
+	OrderSN                string `url:"order_sn"`
+	ResponseOptionalFields string `url:"response_optional_fields"`
+}
+
+type GetTrackingNumberResponse struct {
+	BaseResponse
+
+	Response GetTrackingNumberResponseData `json:"response"`
+}
+
+type GetTrackingNumberResponseData struct {
+	TrackingNumber          string `json:"tracking_number"`
+	PlpNumber               string `json:"plp_number"`
+	FirstMileTrackingNumber string `json:"first_mile_tracking_number"`
+	LastMileTrackingNumber  string `json:"last_mile_tracking_number"`
+	Hint                    string `json:"hint"`
+}
+
+func (s *LogisticsServiceOp) GetTrackingNumber(sid uint64, ordersn string, fields []string, tok string) (*GetTrackingNumberResponse, error) {
+	path := "/logistics/get_tracking_number"
+	opt := GetTrackingNumberRequest{
+		OrderSN:                ordersn,
+		ResponseOptionalFields: strings.Join(fields, ","),
+	}
+
+	resp := new(GetTrackingNumberResponse)
+	err := s.client.WithShop(sid, tok).Get(path, resp, opt)
 	return resp, err
 }
 
 type ShipOrderRequest struct {
-	OrderSN string `json:"order_sn"`
-	PackageNumber string `json:"package_number"`
-	Pickup *ShipOrderRequestPickup `json:"pickup,omitempty"`
-	Dropoff *ShipOrderRequestDropoff `json:"dropoff,omitempty"`
+	OrderSN       string                         `json:"order_sn"`
+	PackageNumber string                         `json:"package_number"`
+	Pickup        *ShipOrderRequestPickup        `json:"pickup,omitempty"`
+	Dropoff       *ShipOrderRequestDropoff       `json:"dropoff,omitempty"`
 	NonIntegrated *ShipOrderRequestNonIntegrated `json:"non_integrated,omitempty"`
 }
 
@@ -152,14 +186,14 @@ type ShipOrderRequestNonIntegrated struct {
 }
 
 type ShipOrderRequestDropoff struct {
-	BranchID uint64 `json:"branch_id"`
+	BranchID       uint64 `json:"branch_id"`
 	SenderRealName string `json:"sender_real_name"`
 	TrackingNumber string `json:"tracking_number"`
 }
 
 type ShipOrderRequestPickup struct {
-	AddressID uint64 `json:"address_id"`
-	PickupTimeID string `json:"pickup_time_id"`
+	AddressID      uint64 `json:"address_id"`
+	PickupTimeID   string `json:"pickup_time_id"`
 	TrackingNumber string `json:"tracking_number"`
 }
 
@@ -167,13 +201,13 @@ type ShipOrderResponse struct {
 	BaseResponse
 }
 
-func (s *LogisticsServiceOp)ShipOrder(sid uint64, data ShipOrderRequest, tok string)(*ShipOrderResponse,error){
+func (s *LogisticsServiceOp) ShipOrder(sid uint64, data ShipOrderRequest, tok string) (*ShipOrderResponse, error) {
 	path := "/logistics/ship_order"
 	resp := new(ShipOrderResponse)
-	req,err:=StructToMap(data)
-	if err!=nil {
-		return nil,err
+	req, err := StructToMap(data)
+	if err != nil {
+		return nil, err
 	}
-	err = s.client.WithShop(sid,tok).Post(path, req, resp)
+	err = s.client.WithShop(sid, tok).Post(path, req, resp)
 	return resp, err
 }
